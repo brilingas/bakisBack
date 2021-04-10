69fc6b26b3829ac514e55bbefc91402e97ce50a1
brilingas <lauris652@gmail.com>
2021-04-10 17:52:36 +0300

    CORS fix

--- src/main/java/guestregistry-controller/src/main/java/lt/brilingas/guestregistry/controller/controllers/PersonController.java
+++ src/main/java/guestregistry-controller/src/main/java/lt/brilingas/guestregistry/controller/controllers/PersonController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/persons")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PersonController {
     @Autowired
     private IPersonService personService;

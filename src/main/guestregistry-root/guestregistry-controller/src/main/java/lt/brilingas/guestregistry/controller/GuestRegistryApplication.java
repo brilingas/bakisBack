b7fd2691c375e68563f08b181c8617a8aa8e18fa
brilingas <lauris652@gmail.com>
2021-11-06 20:29:07 +0200

    refactoring structure

--- src/main/guestregistry-root/guestregistry-controller/src/main/java/lt/brilingas/guestregistry/controller/GuestRegistryApplication.java
+++ src/main/guestregistry-root/guestregistry-controller/src/main/java/lt/brilingas/guestregistry/controller/GuestRegistryApplication.java
@@ -2,11 +2,10 @@ package lt.brilingas.guestregistry.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "lt.brilingas.guestregistry")
-public class GuestRegistry202009Application {
+public class GuestRegistryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(GuestRegistry202009Application.class, args);
+        SpringApplication.run(GuestRegistryApplication.class, args);
     }
 }
--- src/main/guestregistry-root/guestregistry-controller/src/test/java/lt/brilingas/guestregistry/controller/GuestRegistry202009ApplicationTest.java
+++ src/main/guestregistry-root/guestregistry-controller/src/test/java/lt/brilingas/guestregistry/controller/GuestRegistry202009ApplicationTest.java
@@ -3,7 +3,7 @@ package lt.brilingas.guestregistry.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = {GuestRegistry202009Application.class})
+@SpringBootTest(classes = {GuestRegistryApplication.class})
 public class GuestRegistry202009ApplicationTest {
     @Test
     void applicationContextLoads() {
